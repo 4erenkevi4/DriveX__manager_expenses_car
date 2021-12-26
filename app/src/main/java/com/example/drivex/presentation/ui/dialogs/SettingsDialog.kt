eecc785c507672612596753683a1a98325cc02fa
Anton <acherenkevich@elinext.com>
2021-12-26 21:53:16 +0300

    (FIX)Added settings fragment

---
+++ app/src/main/java/com/example/drivex/presentation/ui/dialogs/SettingsDialog.kt
@@ -0,0 +1,54 @@
+package com.example.drivex.presentation.ui.dialogs
+
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.setFragmentResult
+import com.example.drivex.R
+
+
+class SettingsDialog(var arrayItems: Array<String>, val type : String) : DialogFragment() {
+
+    companion object{
+        const val TYPE_VOLUME= "volume"
+        const val TYPE_CONSUMPTION= "consumption"
+        const val TYPE_CURENCY= "currency"
+    }
+
+    val prefs: SharedPreferences? = context?.getSharedPreferences(
+        "com.drivex.app", Context.MODE_PRIVATE
+    )
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val selectedItems = ArrayList<Int>() // Where we track the selected items
+            val builder = AlertDialog.Builder(it)
+            builder.setTitle(R.string.select_unit_settings)
+                .setSingleChoiceItems(
+                    arrayItems, -1
+                ) { dialog, item ->
+                    parentFragmentManager.setFragmentResult("requestKey", bundleOf(type to arrayItems[item]))
+                    Toast.makeText(
+                        activity, "${R.string.selected_general} ${arrayItems[item]}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                .setPositiveButton(
+                    "OK"
+                ) { dialog, id ->
+                    // User clicked OK, so save the selectedItems results somewhere
+                    // or return them to the component that opened the dialog
+                }
+                .setNegativeButton(R.string.cancel_general) { dialog, id ->
+                }
+
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+}
--- app/src/main/java/com/example/drivex/presentation/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/example/drivex/presentation/ui/setting/SettingFragment.kt
@@ -1,31 +1,134 @@
 package com.example.drivex.presentation.ui.setting
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Spinner
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProvider
 import com.example.drivex.R
+import com.example.drivex.presentation.ui.dialogs.SettingsDialog
+import com.example.drivex.presentation.ui.dialogs.SettingsDialog.Companion.TYPE_CONSUMPTION
+import com.example.drivex.presentation.ui.dialogs.SettingsDialog.Companion.TYPE_CURENCY
+import com.example.drivex.presentation.ui.dialogs.SettingsDialog.Companion.TYPE_VOLUME
+import java.util.*
+
 
 class SettingFragment : Fragment() {
 
     private lateinit var settingViewModel: SettingViewModel
+    private lateinit var carVendor: Spinner
+    private lateinit var carModel: EditText
+    private lateinit var soundStartApp: SwitchCompat
+    private lateinit var utilsVolume: LinearLayout
+    private lateinit var utilsFuel: LinearLayout
+    private lateinit var utilsCurrency: LinearLayout
+    private lateinit var currency: TextView
+    private lateinit var volume: TextView
+    private lateinit var fuel_cons: TextView
+    val prefs: SharedPreferences? = context?.getSharedPreferences(
+        "com.drivex.app", Context.MODE_PRIVATE
+    )
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        settingViewModel =
-                ViewModelProvider(this).get(SettingViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_setting, container, false)
-        val textView: TextView = root.findViewById(R.id.textView7)
-        settingViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        settingViewModel = ViewModelProvider(this).get(SettingViewModel::class.java)
+        return inflater.inflate(R.layout.fragment_setting, container, false)
     }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    @SuppressLint("ResourceType")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        carVendor = view.findViewById(R.id.car_vendor)
+        carModel = view.findViewById(R.id.Car_model)
+        soundStartApp = view.findViewById(R.id.switch_sound)
+        utilsVolume = view.findViewById(R.id.volume_picker_setting)
+        utilsFuel = view.findViewById(R.id.fuel_picker_setting)
+        utilsCurrency = view.findViewById(R.id.currency_picker_setting)
+        currency = utilsCurrency.findViewById(R.id.curency)
+        volume = utilsVolume.findViewById(R.id.volume)
+        fuel_cons = utilsFuel.findViewById(R.id.fuel_settings)
+        utilsVolume.setOnClickListener {
+            createDialog(
+                arrayOf(
+                    getString(R.string.liter),
+                    getString(R.string.Gallon)
+                ), TYPE_VOLUME
+            )
+        }
+        utilsFuel.setOnClickListener {
+            createDialog(
+                arrayOf(
+                    getString(R.string.l_km),
+                    getString(R.string.l_mile),
+                    getString(R.string.g_km),
+                    getString(R.string.g_mile)
+                ), TYPE_CONSUMPTION
+            )
+        }
+        currency.text = getCurrency(Locale.getDefault())
+        utilsCurrency.setOnClickListener {
+            createDialog(
+                arrayOf(
+                    getCurrency(Locale.US),
+                    getCurrency(Locale.GERMANY),
+                    getCurrency(Locale.PRC),
+                    getCurrency(Locale.UK),
+                    getCurrency(Locale.getDefault())
+                ), TYPE_CURENCY
+            )
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        childFragmentManager.setFragmentResultListener("requestKey", this) { key, bundle ->
+            bundle.getString(TYPE_VOLUME).let {it?.setText(volume) }
+            bundle.getString(TYPE_CURENCY).let { it?.setText(currency)
+            }
+            bundle.getString(TYPE_CONSUMPTION).let {it?.setText(fuel_cons) }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    fun getCurrency(locale: Locale): String {
+        val currency: Currency = Currency.getInstance(locale)
+        return currency.displayName + " " + currency.symbol
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    fun saveToSP(string: String) {
+        val editor: SharedPreferences.Editor? = prefs?.edit()
+        editor?.putString("VOLUME", string)
+    }
+
+    private fun createDialog(array: Array<String>, type: String) {
+        val myDialogFragment = SettingsDialog(array, type)
+        val manager: FragmentManager = childFragmentManager
+        val transaction: FragmentTransaction = manager.beginTransaction()
+        myDialogFragment.show(transaction, "dialog")
+    }
+
+}
+
+private fun String.setText(textview: TextView) {
+    if (this.isEmpty().not())
+        textview.text = this
 }
