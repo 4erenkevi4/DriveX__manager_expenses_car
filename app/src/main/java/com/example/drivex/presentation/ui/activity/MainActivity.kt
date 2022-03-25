29ae7bec05e277cdb8a8c42e4b46ab1a7003f369
Anton <acherenkevich@elinext.com>
2022-03-25 15:43:59 +0200

    DEV Final implemented filters

--- app/src/main/java/com/example/drivex/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/drivex/presentation/ui/activity/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.drivex.presentation.ui.activity
 
 import android.Manifest
-import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
@@ -10,11 +9,9 @@ import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
-import androidx.core.view.isVisible
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
@@ -24,7 +21,6 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.drivex.R
 import com.example.drivex.presentation.ui.dialogs.SettingsDialog.Companion.TYPE_SOUND
-import com.example.drivex.presentation.ui.setting.SettingFragment
 import com.example.drivex.utils.Constans
 import com.example.drivex.utils.Constans.IS_PAYMENT
 import com.example.drivex.utils.Constans.IS_REFUEL
@@ -67,11 +63,12 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-        if(prefs.contains(TYPE_SOUND)){
-            startMusic(prefs.getBoolean(TYPE_SOUND,false))
+        if (prefs.contains(TYPE_SOUND)) {
+            startMusic(prefs.getBoolean(TYPE_SOUND, false))
         }
         requestPermissions()
     }
+
     override fun onBackPressed() {
         val count = supportFragmentManager.backStackEntryCount
         if (count == 0) {
@@ -85,115 +82,116 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 3 && resultCode == RESULT_OK) {
             val imageUri = data?.data
-            val args = bundleOf(Pair("IMAGE_URI",imageUri))
-            replaceFragment(R.id.action_global_nav_settings,args)
+            val args = bundleOf(Pair("IMAGE_URI", imageUri))
+            replaceFragment(R.id.action_global_nav_settings, args)
         }
     }
 
-        private fun requestPermissions() {
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ) == PackageManager.PERMISSION_DENIED
-            ) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                    ActivityCompat.requestPermissions(
-                        this, arrayOf(
-                            Manifest.permission.READ_EXTERNAL_STORAGE,
-                            Manifest.permission.CAMERA,
-                            Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.ACCESS_COARSE_LOCATION,
-                            Manifest.permission.FOREGROUND_SERVICE
-                        ),
-                        Constans.REQUEST_CODE_GET_PERMISSION
-                    )
-                } else {
-                    ActivityCompat.requestPermissions(
-                        this, arrayOf(
-                            Manifest.permission.READ_EXTERNAL_STORAGE,
-                            Manifest.permission.CAMERA,
-                            Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.ACCESS_COARSE_LOCATION,
-                            Manifest.permission.ACCESS_BACKGROUND_LOCATION
-                        ),
-                        Constans.REQUEST_CODE_GET_PERMISSION
-                    )
-                }
-            }
-        }
-
-        override fun onRequestPermissionsResult(
-            requestCode: Int,
-            permissions: Array<out String>,
-            grantResults: IntArray
+    private fun requestPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_DENIED
         ) {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-            when (requestCode) {
-                1 -> {
-                    if (grantResults.isNotEmpty()
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
-                    ) {
-                        Toast.makeText(
-                            this@MainActivity,
-                            " Permissions was granted.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.FOREGROUND_SERVICE
+                    ),
+                    Constans.REQUEST_CODE_GET_PERMISSION
+                )
+            } else {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                    ),
+                    Constans.REQUEST_CODE_GET_PERMISSION
+                )
+            }
+        }
+    }
 
-                        Toast.makeText(
-                            this@MainActivity,
-                            "Permission denied to read your External storage",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                    return
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty()
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        " Permissions was granted.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Permission denied to read your External storage",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
+                return
             }
         }
+    }
 
-        private fun startMusic(isNeedSound: Boolean) {
-            if (isNeedSound) {
-                playerStart = MediaPlayer.create(this, R.raw.engine_start)
-                playerStart.start()
-            }
+    private fun startMusic(isNeedSound: Boolean) {
+        if (isNeedSound) {
+            playerStart = MediaPlayer.create(this, R.raw.engine_start)
+            playerStart.start()
         }
+    }
 
-        override fun onSupportNavigateUp(): Boolean {
-            val navController = findNavController(R.id.nav_host_fragment)
-            return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-        }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
 
-        private fun setBoomMenu() {
-            val intentFuell = Intent(this, FuelActivity::class.java)
-            val intentService = Intent(this, ServiceActivity::class.java)
-            val intentMap = Intent(this, MapsActivity::class.java)
-            val buttonFuel = TextOutsideCircleButton.Builder()
-                .normalImageRes(R.drawable.ic_car)
-                .normalText(getString(R.string.refuel))
-                .listener { startActivity(intentFuell.putExtra(PAYMENT_TYPE, IS_REFUEL)) }
-            val buttonService = TextOutsideCircleButton.Builder()
-                .normalImageRes(R.drawable.ic_service)
-                .normalText(getString(R.string.add_service))
-                .listener { startActivity(intentService) }
-            val buttonPayments = TextOutsideCircleButton.Builder()
-                .normalImageRes(R.drawable.ic_money)
-                .normalText(getString(R.string.payment))
-                .listener { startActivity(intentFuell.putExtra(PAYMENT_TYPE, IS_PAYMENT)) }
-            val buttonExpenses = TextOutsideCircleButton.Builder()
-                .normalImageRes(R.drawable.ic_shopping)
-                .normalText(getString(R.string.your_buy))
-                .listener { startActivity(intentFuell.putExtra(PAYMENT_TYPE, IS_SHOPPING)) }
-            val buttonDriving = TextOutsideCircleButton.Builder()
-                .normalImageRes(R.drawable.ic_map)
-                    .normalText(getString(R.string.add_ride))
-                .listener { startActivity(intentMap) }
-                boomMenu!!.builders =
-                    arrayListOf(buttonFuel,buttonService,buttonPayments,buttonExpenses,buttonDriving )
-            }
-    fun replaceFragment(navID: Int, args: Bundle? = null){
+    private fun setBoomMenu() {
+        val intentFuell = Intent(this, FuelActivity::class.java)
+        val intentService = Intent(this, ServiceActivity::class.java)
+        val intentMap = Intent(this, MapsActivity::class.java)
+        val buttonFuel = TextOutsideCircleButton.Builder()
+            .normalImageRes(R.drawable.ic_car)
+            .normalText(getString(R.string.refuel))
+            .listener { startActivity(intentFuell.putExtra(PAYMENT_TYPE, IS_REFUEL)) }
+        val buttonService = TextOutsideCircleButton.Builder()
+            .normalImageRes(R.drawable.ic_service)
+            .normalText(getString(R.string.add_service))
+            .listener { startActivity(intentService) }
+        val buttonPayments = TextOutsideCircleButton.Builder()
+            .normalImageRes(R.drawable.ic_money)
+            .normalText(getString(R.string.payment))
+            .listener { startActivity(intentFuell.putExtra(PAYMENT_TYPE, IS_PAYMENT)) }
+        val buttonExpenses = TextOutsideCircleButton.Builder()
+            .normalImageRes(R.drawable.ic_shopping)
+            .normalText(getString(R.string.your_buy))
+            .listener { startActivity(intentFuell.putExtra(PAYMENT_TYPE, IS_SHOPPING)) }
+        val buttonDriving = TextOutsideCircleButton.Builder()
+            .normalImageRes(R.drawable.ic_map)
+            .normalText(getString(R.string.add_ride))
+            .listener { startActivity(intentMap) }
+        boomMenu!!.builders =
+            arrayListOf(buttonFuel, buttonService, buttonPayments, buttonExpenses, buttonDriving)
+    }
+
+    fun replaceFragment(navID: Int, args: Bundle? = null) {
         val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-        navController.navigate(navID,args)
+        navController.navigate(navID, args)
     }
 
 
-    }
+}
